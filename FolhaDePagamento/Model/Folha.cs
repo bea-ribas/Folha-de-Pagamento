6b7284a157892925f75a67cfe9e21cf343e419cd
Beatriz Ribas <beatriz.ribas09@hotmail.com>
2017-04-17 21:44:43 -0300

    Update Folha.cs

--- FolhaDePagamento/Model/Folha.cs
+++ FolhaDePagamento/Model/Folha.cs
@@ -8,16 +8,19 @@ namespace FolhaDePagamento.Model
 {
     class Folha
     {
-        private int Folha { get; set; }
-        private double Salario { get; set; }
-        private double Inss { get; set;
-        private double ImpostoDeRenda{ get;set;}
-        private double Fgts {get; set;}
-
-        Folha Folha = new Folha();
-        Salario salario = new Salario();
-        Inss inss = new Inss();
-        ImpostoDeRenda imposto = new ImpostoDeRenda();
-        Fgts fgts = new Fgts();
+        public int Folha { get; set; }
+        public double Salario { get; set; }
+        public double Inss { get; set;
+        public double ImpostoDeRenda{ get;set;}
+        public double Fgts {get; set;}
+        
+        public Folha ()
+        {
+            Folha Folha = new Folha();
+            Salario salario = new Salario();
+            Inss inss = new Inss();
+            ImpostoDeRenda imposto = new ImpostoDeRenda();
+            Fgts fgts = new Fgts();
+        }
     }
 }
